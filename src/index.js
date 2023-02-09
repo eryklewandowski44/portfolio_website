d677f77005315ec9c85c58574dae14ba2a15b611
Himanshu Rathore <himanshu.rathore2561@gmail.com>
2023-02-09 19:34:59 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import ScrollToTop from './ScrollToTop'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Router>
     <React.StrictMode>
+      <ScrollToTop/>
       <App />
     </React.StrictMode>
   </Router>
