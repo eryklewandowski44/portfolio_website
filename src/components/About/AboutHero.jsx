45ec86c41afd9be3d9e1b3b8dd914707cb9b7443
Himanshu Rathore <himanshu.rathore2561@gmail.com>
2023-02-09 23:02:45 +0530

    Cap-finland

--- src/components/About/AboutHero.jsx
+++ src/components/About/AboutHero.jsx
@@ -4,7 +4,7 @@ function AboutHero() {
     return (
         <div className='about-contain'>
             <h1>GM 👋</h1>
-            <p>I am a twenty-two year old marketing and web3 enthusiast. Originally from Poland, currently living in finland. I graduated Management and Law at Adam Mickiewicz University and Law at UEF. You will ask how did I go from law to marketing? Well, from the beginning of my studies I discovered my love for marketing and since then, that is more than 3 years, I have been gaining experience. I have worked in many places, through global corporations, to various startups to my own business.</p>
+            <p>I am a twenty-two year old marketing and web3 enthusiast. Originally from Poland, currently living in Finland. I graduated Management and Law at Adam Mickiewicz University and Law at UEF. You will ask how did I go from law to marketing? Well, from the beginning of my studies I discovered my love for marketing and since then, that is more than 3 years, I have been gaining experience. I have worked in many places, through global corporations, to various startups to my own business.</p>
             <p>I want to build an exciting new world as a web3 builder. My latest project is a Rens x The Sandbox collaboration. The results of the campaign were very stunning, and the moment when I saw satisfied fans bragging about their NFTs, characters in the metaverse or physical items was indescribable to me.</p>
             <p>With projects I always give all of myself. I often manage international teams, so I know how to adjust cooperation strategies. Besides, I like to optimize project processes with the help of many modern tools and AI.</p>
             <p>Don't hesitate to contact me :)</p>
