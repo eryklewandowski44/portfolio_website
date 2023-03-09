77ca19a82946ff6c3e8ee1338c5ec431b99d0ae3
eryklewandowski44 <122271759+eryklewandowski44@users.noreply.github.com>
2023-03-09 16:52:39 +0200

    Update AboutHero.jsx

--- src/components/About/AboutHero.jsx
+++ src/components/About/AboutHero.jsx
@@ -5,8 +5,8 @@ function AboutHero() {
         <div className='about-contain'>
             <h1>GM 👋</h1>
             <p>I am a twenty-two year old digital marketing enthusiast. Originally from Poland, currently living in Finland. I graduated Management with Marketing Specialisation and Businesses Law at Adam Mickiewicz University and Law at University of Eastern Finland. You will ask how did I go from law to marketing? Well, from the beginning of my studies I discovered my love for marketing and since then, that is more than 3 years, I have been gaining experience. I have worked in many places, through global corporations, to various startups to my own business.</p>
-            <p>I want to create new creative awesome campaigns in digital world. My latest project is a Rens x The Sandbox collaboration. The results of the campaign were very stunning, and the moment when I saw satisfied fans bragging about their NFTs, elements of characters that we designed with team or physical items was indescribable to me.</p>
-            <p>With projects I always give all of myself. I often manage international teams, so I know how to adjust cooperation strategies. Besides, I like to optimize project processes with the help of many modern tools and AI.</p>
+            <p>My expertise extends to managing entire marketing campaigns from inception to completion. I am capable of creating digital assets, content, and effectively leading a team and whole communities, while also utilizing outsourcing to achieve optimal results. Additionally, I am proficient in utilizing a range of tools to create graphics, design, and manage websites, such as Figma, Webflow, Wordpress, Adobe, iMovie, Canva, and LighTracer.</p>
+            <p>While I have achieved already many experience in my young career, I still aspire to learn and grow under the guidance of more experienced mentors in the industry. I have a true passion for digital marketing and with my knowledge and skilled team of specialists, I am confident in achieving great results.</p>
             <p>Don't hesitate to contact me :)</p>
         </div>
     )
